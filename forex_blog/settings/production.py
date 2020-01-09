82853e28a1da7470c265ffbb39b1e6a7a4bf9474
thepsalmist <xf.xavierfrank@gmail.com>
2020-01-09 16:50:14 +0300

    change template

--- forex_blog/settings/production.py
+++ forex_blog/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["176.58.99.51", "www.myhost.com"]
+ALLOWED_HOSTS = ["176.58.99.51", "127.0.0.1"]
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
