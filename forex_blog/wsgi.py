c01e60091e9240fdeff856cf525c2e1759d4b1b1
thepsalmist <76.thealchemist@gmail.com>
2020-01-10 07:30:22 +0000

    change production settings

--- forex_blog/wsgi.py
+++ forex_blog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forex_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forex_blog.settings.production')
 
 application = get_wsgi_application()
